e60511c0748c4cb36cf08d7a9eeb1e8d5625f620
Anna Franz <franz.karols@gmail.com>
2025-05-26 22:50:45 -0300

    fix: variavel ambiente

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://product-manager-backend.fly.dev';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://product-manager-little-wave-7617.fly.dev';
 
 export const api = {
   baseURL: API_URL,
